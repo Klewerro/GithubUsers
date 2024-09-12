9dadc885c0ed1bc9cecb2db6547dc122c4bd175a
Klewerro <pol.szym95@gmail.com>
2024-09-12 23:19:00 +0200

    RemoteMediator for search for users created

--- app/src/main/java/com/klewerro/githubusers/users/domain/UserRepository.kt
+++ app/src/main/java/com/klewerro/githubusers/users/domain/UserRepository.kt
@@ -1,5 +1,13 @@
 package com.klewerro.githubusers.users.domain
 
+import androidx.paging.PagingData
+import com.klewerro.githubusers.users.domain.model.User
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
 interface UserRepository {
-    suspend fun getUsers()
+    val searchQueryFlow: StateFlow<String>
+    val searchPager: Flow<PagingData<User>>
+
+    fun updateSearchQuery(searchQuery: String)
 }
