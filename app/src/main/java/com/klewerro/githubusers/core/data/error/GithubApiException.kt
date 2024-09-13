363e57251054de856a1e62f148d96b95926b3012
Klewerro <pol.szym95@gmail.com>
2024-09-14 01:45:58 +0200

    Showing user information when time limit will be reset.
    When user call api more that 10 times per minute, GithubApiException of type RATE_LIMIT_EXCEEDED will be thrown. Added time of next refresh fetched from headers, which will propagated to the UI.

--- app/src/main/java/com/klewerro/githubusers/core/data/error/GithubApiException.kt
+++ app/src/main/java/com/klewerro/githubusers/core/data/error/GithubApiException.kt
@@ -2,8 +2,10 @@ package com.klewerro.githubusers.core.data.error
 
 class GithubApiException(
     val errorType: GithubApiErrorType,
-    val originalException: Throwable? = null
+    val originalException: Throwable? = null,
+    override val message: String = "An error occurred during request: $errorType",
+    val additionalInformation: String? = null
 ) : Exception(
-    "An error occurred during request: $errorType",
+    message,
     originalException
 )
