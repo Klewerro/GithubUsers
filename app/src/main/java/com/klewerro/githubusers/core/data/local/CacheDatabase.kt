49a59c9b92fabb78a93354b229c9010a93719234
Klewerro <pol.szym95@gmail.com>
2024-09-15 00:25:53 +0200

    Implemented local data layer of user's github repositories

--- app/src/main/java/com/klewerro/githubusers/core/data/local/CacheDatabase.kt
+++ app/src/main/java/com/klewerro/githubusers/core/data/local/CacheDatabase.kt
@@ -2,16 +2,20 @@ package com.klewerro.githubusers.core.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.klewerro.githubusers.userDetails.data.local.GithubRepositoryDao
+import com.klewerro.githubusers.userDetails.data.local.GithubRepositoryEntity
 import com.klewerro.githubusers.users.data.local.UserDao
 import com.klewerro.githubusers.users.data.local.UserEntity
 
 @Database(
     entities = [
-        UserEntity::class
+        UserEntity::class,
+        GithubRepositoryEntity::class
     ],
     version = 1,
     exportSchema = false
 )
 abstract class CacheDatabase : RoomDatabase() {
     abstract val userDao: UserDao
+    abstract val githubRepositoryDao: GithubRepositoryDao
 }
---
+++ app/src/main/java/com/klewerro/githubusers/userDetails/data/local/GithubRepositoryEntity.kt
@@ -0,0 +1,32 @@
+package com.klewerro.githubusers.userDetails.data.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "github_repository",
+    indices = [
+        Index(value = ["user_id"])
+    ]
+)
+data class GithubRepositoryEntity(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val description: String?,
+    val url: String,
+    @ColumnInfo(name = "html_url")
+    val htmlUrl: String,
+    @ColumnInfo(name = "created_at")
+    val createdAt: String,
+    val language: String?,
+    @ColumnInfo(name = "pushed_at")
+    val pushedAt: String,
+    @ColumnInfo(name = "stargazers_count")
+    val stargazersCount: Int,
+    @ColumnInfo(name = "watchers_count")
+    val watchersCount: Int,
+    @ColumnInfo(name = "user_id")
+    val userId: Int
+)
