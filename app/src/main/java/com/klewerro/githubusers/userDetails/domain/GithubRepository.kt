2928f6217c1c8f86b247874f58d5cb058d1863d4
Klewerro <pol.szym95@gmail.com>
2024-09-15 00:28:00 +0200

    GithubRepositoryRepository joining remote and local dataSources implemented. GithubRepository domain model and DI setup

---
+++ app/src/main/java/com/klewerro/githubusers/userDetails/domain/GithubRepository.kt
@@ -0,0 +1,14 @@
+package com.klewerro.githubusers.userDetails.domain
+
+data class GithubRepository(
+    val id: Int,
+    val name: String,
+    val description: String?,
+    val url: String,
+    val htmlUrl: String,
+    val createdAt: String,
+    val language: String?,
+    val pushedAt: String,
+    val stargazersCount: Int,
+    val watchersCount: Int
+)
