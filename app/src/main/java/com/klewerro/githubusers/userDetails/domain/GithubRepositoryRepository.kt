2e5175449083a3ce688a6664cb6caa952ebe2745
Klewerro <pol.szym95@gmail.com>
2024-09-15 00:28:00 +0200

    GithubRepositoryRepository joining remote and local dataSources implemented. GithubRepository domain model and DI setup

---
+++ app/src/main/java/com/klewerro/githubusers/userDetails/domain/GithubRepositoryRepository.kt
@@ -0,0 +1,9 @@
+package com.klewerro.githubusers.userDetails.domain
+
+import kotlinx.coroutines.flow.Flow
+
+interface GithubRepositoryRepository {
+
+    fun observeUserRepositories(userId: Int): Flow<List<GithubRepository>>
+    suspend fun getUserRepositories(userId: Int, login: String)
+}
