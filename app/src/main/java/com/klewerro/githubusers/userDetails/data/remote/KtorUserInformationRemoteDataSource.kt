5c4774b3643c9468333abadbb7c2ae1f6ffa8e2e
Klewerro <pol.szym95@gmail.com>
2024-09-16 17:40:56 +0200

    Getting remote UserDetails informations

--- app/src/main/java/com/klewerro/githubusers/userDetails/data/remote/KtorUserInformationRemoteDataSource.kt
+++ app/src/main/java/com/klewerro/githubusers/userDetails/data/remote/KtorUserInformationRemoteDataSource.kt
@@ -2,13 +2,13 @@ package com.klewerro.githubusers.userDetails.data.remote
 
 import com.klewerro.githubusers.core.data.ApiConstants
 import com.klewerro.githubusers.core.data.runRequestThrowing
-import com.klewerro.githubusers.userDetails.domain.GithubRepositoryRemoteDataSource
+import com.klewerro.githubusers.userDetails.domain.UserInformationRemoteDataSource
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 
-class KtorGithubRepositoryRemoteDataSource(private val httpClient: HttpClient) :
-    GithubRepositoryRemoteDataSource {
+class KtorUserInformationRemoteDataSource(private val httpClient: HttpClient) :
+    UserInformationRemoteDataSource {
 
     override suspend fun getUserRepositories(login: String): List<GithubRepositoryDto> =
         runRequestThrowing {
@@ -16,4 +16,8 @@ class KtorGithubRepositoryRemoteDataSource(private val httpClient: HttpClient) :
                 parameter(ApiConstants.PARAM_SORT, ApiConstants.VALUE_SORT_UPDATED)
             }
         }
+
+    override suspend fun getUserDetails(login: String): UserDetailsDto = runRequestThrowing {
+        httpClient.get(ApiConstants.userDetails(login))
+    }
 }
---
+++ app/src/main/java/com/klewerro/githubusers/users/domain/model/UserDetails.kt
@@ -0,0 +1,21 @@
+package com.klewerro.githubusers.users.domain.model
+
+data class UserDetails(
+    val id: Int,
+    val login: String,
+    val name: String,
+    val location: String,
+    val bio: String,
+    val blog: String,
+    val company: String?,
+    val email: String?,
+    val hireable: Boolean?,
+    val publicGistsCount: Int,
+    val publicReposCount: Int,
+    val followersCount: Int,
+    val followingCount: Int,
+    val createdAt: String,
+    val updatedAt: String,
+    val url: String,
+    val inAppRefreshedAt: Long
+)
