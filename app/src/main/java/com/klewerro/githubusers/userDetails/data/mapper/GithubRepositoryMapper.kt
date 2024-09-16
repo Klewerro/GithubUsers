668f2d690efef2ff34f251dba92f339a483bb4d7
Klewerro <pol.szym95@gmail.com>
2024-09-16 23:48:32 +0200

    Fixed saving and showing formatted date

--- app/src/main/java/com/klewerro/githubusers/userDetails/data/mapper/GithubRepositoryMapper.kt
+++ app/src/main/java/com/klewerro/githubusers/userDetails/data/mapper/GithubRepositoryMapper.kt
@@ -1,10 +1,9 @@
 package com.klewerro.githubusers.userDetails.data.mapper
 
+import com.klewerro.githubusers.core.util.convertIso8601DateToLocalDateTime
 import com.klewerro.githubusers.userDetails.data.local.githubRepository.GithubRepositoryEntity
 import com.klewerro.githubusers.userDetails.data.remote.GithubRepositoryDto
 import com.klewerro.githubusers.userDetails.domain.model.GithubRepository
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 fun GithubRepositoryEntity.toGithubRepository() = GithubRepository(
     id = this.id,
@@ -12,9 +11,9 @@ fun GithubRepositoryEntity.toGithubRepository() = GithubRepository(
     description = this.description,
     url = this.url,
     htmlUrl = this.htmlUrl,
-    createdAt = this.createdAt,
+    createdAt = convertIso8601DateToLocalDateTime(this.createdAt),
+    pushedAt = convertIso8601DateToLocalDateTime(this.pushedAt),
     language = this.language,
-    pushedAt = this.pushedAt,
     stargazersCount = this.stargazersCount,
     watchersCount = this.watchersCount,
     forksCount = this.forksCount,
@@ -27,8 +26,8 @@ fun GithubRepositoryDto.toEntity(userId: Int): GithubRepositoryEntity = GithubRe
     description = this.description,
     url = this.url,
     htmlUrl = this.htmlUrl,
-    createdAt = convertIso8601DateToLocalDateTime(this.createdAt),
-    pushedAt = convertIso8601DateToLocalDateTime(this.pushedAt),
+    createdAt = this.createdAt,
+    pushedAt = this.pushedAt,
     language = this.language,
     stargazersCount = this.stargazersCount,
     watchersCount = this.watchersCount,
@@ -36,14 +35,3 @@ fun GithubRepositoryDto.toEntity(userId: Int): GithubRepositoryEntity = GithubRe
     openIssuesCount = this.openIssuesCount,
     userId = userId
 )
-
-/**
- * Drops last sign (z, meaning UTC time) from ISO 8601 date time text and convert it to
- * Java LocalDateTime.
- */
-private fun convertIso8601DateToLocalDateTime(iso8601DateTimeText: String): String {
-    val date = LocalDateTime.parse(iso8601DateTimeText.dropLast(1))
-    val formatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm")
-    val formattedDate = date.format(formatter)
-    return formattedDate
-}
