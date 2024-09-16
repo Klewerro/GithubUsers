3da4444fb8580328b6ddd1bed9c7ba1fde2470b4
Klewerro <pol.szym95@gmail.com>
2024-09-16 14:15:33 +0200

    UserDetailsViewModel tests for getting data from correct data source

--- app/src/main/java/com/klewerro/githubusers/core/util/testData/GithubRepositoryTestData.kt
+++ app/src/main/java/com/klewerro/githubusers/core/util/testData/GithubRepositoryTestData.kt
@@ -20,4 +20,32 @@ object GithubRepositoryTestData {
         forksCount = 4,
         openIssuesCount = 2
     )
+    val repository2 = GithubRepository(
+        id = 2,
+        name = "Repository2",
+        description = "Example not popular repo description.",
+        url = "https://repo2.com",
+        htmlUrl = "https://repo2.com",
+        createdAt = "22-12-23 15:39",
+        pushedAt = "01-11-24 15:11",
+        language = "Kotlin",
+        stargazersCount = 0,
+        watchersCount = 0,
+        forksCount = 0,
+        openIssuesCount = 0
+    )
+    val repository3 = GithubRepository(
+        id = 3,
+        name = "Example very popular repository.",
+        description = ".",
+        url = "https://repo3.com",
+        htmlUrl = "https://repo3.com",
+        createdAt = "12-01-20 10:09",
+        pushedAt = "03-12-24 11:11",
+        language = "Kotlin",
+        stargazersCount = 10_002,
+        watchersCount = 8_009,
+        forksCount = 1218,
+        openIssuesCount = 327
+    )
 }
---
+++ app/src/test/java/com/klewerro/githubusers/fake/FakeGithubRepositoryRepository.kt
@@ -0,0 +1,75 @@
+package com.klewerro.githubusers.fake
+
+import com.klewerro.githubusers.core.data.error.GithubApiErrorType
+import com.klewerro.githubusers.core.data.error.GithubApiException
+import com.klewerro.githubusers.core.util.testData.GithubRepositoryTestData
+import com.klewerro.githubusers.core.util.testData.UserTestData
+import com.klewerro.githubusers.userDetails.domain.GithubRepository
+import com.klewerro.githubusers.userDetails.domain.GithubRepositoryRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+
+class FakeGithubRepositoryRepository(
+    var userHaveAnyRepository: Boolean,
+    var defaultDelay: Long = 1_000L,
+    var getUserRepositoriesReturnSuccess: Boolean = true,
+    var savedGithubRepositories: List<GithubRepository> = listOf(
+        GithubRepositoryTestData.repository1,
+        GithubRepositoryTestData.repository2,
+        GithubRepositoryTestData.repository3
+    ),
+    var user1Repos: List<GithubRepository> = savedGithubRepositories.take(2),
+    var user2Repos: List<GithubRepository> = listOf(savedGithubRepositories.last())
+) : GithubRepositoryRepository {
+
+    var wasGetUserRepositoriesCalled: Boolean = false
+
+    private val flow = MutableStateFlow<Map<Int, List<GithubRepository>>>(emptyMap())
+
+    override fun observeUserRepositories(userId: Int): Flow<List<GithubRepository>> {
+        val repos = when (userId) {
+            UserTestData.user1.id -> if (userHaveAnyRepository) user1Repos else emptyList()
+            UserTestData.user2.id -> if (userHaveAnyRepository) user2Repos else emptyList()
+            else -> emptyList()
+        }
+
+        flow.update {
+            it.plus(userId to repos)
+        }
+
+        return flow.map { it[userId] ?: emptyList() }
+    }
+
+    /**
+     * Only triggers refresh in httpClient and returning data in database flow,
+     * so only delay in test function.
+     */
+    override suspend fun getUserRepositories(userId: Int, login: String) {
+        wasGetUserRepositoriesCalled = true
+//        Log.d(TAG, "getUserRepositories(): fetch started")
+        delay(defaultDelay)
+//        Log.d(TAG, "getUserRepositories(): fetched")
+
+        val repos = when (userId) {
+            UserTestData.user1.id -> if (getUserRepositoriesReturnSuccess) user1Repos else throw GithubApiException(errorType = GithubApiErrorType.UNKNOWN_ERROR)
+            UserTestData.user2.id -> if (getUserRepositoriesReturnSuccess) user2Repos else throw GithubApiException(errorType = GithubApiErrorType.UNKNOWN_ERROR)
+            else -> emptyList()
+        }
+
+        flow.update {
+            it.plus(userId to repos)
+        }
+    }
+
+    override suspend fun userHaveAnyRepository(userId: Int): Boolean {
+        val result = when (userId) {
+            UserTestData.user1.id -> userHaveAnyRepository ?: true
+            UserTestData.user2.id -> userHaveAnyRepository ?: true
+            else -> userHaveAnyRepository ?: false
+        }
+        return result
+    }
+}
